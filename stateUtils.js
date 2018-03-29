fb86fe55b6d81bd8eb91729d28487bee1fc6d40e
changaimaraSA <changa.imara@sonomaacademy.org>
2018-03-29 13:32:55 -0700

    added getNumberOfCards and getTheme functions to stateUtils

--- stateUtils.js
+++ stateUtils.js
@@ -1,3 +1,11 @@
 function getDeck(state){
     return state.gameState.deck;
 }
+
+function getNumberOfCards(state){
+    return state.options.numberOfCards;
+}
+
+function getTheme(state){
+    return state.options.theme
+}
